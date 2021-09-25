75fc1f023112e12c3059af51f73562ad6bf81495
Julio Cavalin <julio.cavalin@gmail.com>
2021-09-25 17:19:26 -0300

    feat: Add first feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -8,11 +9,9 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!!');
+client.on('message', msg => {
+  if (msg.content.trim().toLocaleLowerCase() === 'desculpa bia') {
+    msg.reply('RAPARIGA!');
   }
 });
 
